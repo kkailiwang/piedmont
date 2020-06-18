b44e7741848fe639e28947decdfff53f3e1fc780
kailiwang <kailiwang2018@gmail.com>
2020-06-18 01:01:10 -0500

    aowiegjawojefg

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ axios
     // ["6911336", "Kaili", "Wang", "$6.00", "2020-06-11 2:00:00", "FALSE", "comment goes here"]
 
     rows = rows.map(row => {
-      let day = new Date(row[DATE].replace(' ', 'T'));
+      // console.log(row[DATE].replace(' ', 'T'))
+      const dateString = row[DATE].length < 19 ? row[DATE].replace(' ', 'T0') : row[DATE].replace(' ', 'T');
+      let day = new Date(dateString);
+      console.log(day);
       let anon = row[ANONYMOUS] === "TRUE" ? true : false;
 
       let newRow = row;
